b6597cdb78d93b894a1a728ef4605647f00c9771
derk1021 <derekbl@uci.edu>
2021-06-28 19:19:17 -0700

    saving changes

---
+++ emotionalgifs/index.js
@@ -0,0 +1,15 @@
+var multipart = require('parse-multipart');
+module.exports = async function (context, req) {
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    
+    var body = req.body;
+
+    var parts = multipart.Parse(body, boundary);
+
+    let base64data = Buffer.from(parts[0].data).toString('base64')
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: base64data
+    };
+}
