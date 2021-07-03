e0443f661c3da1c0d1975d23bb601f105a6feb43
derk1021 <derekbl@uci.edu>
2021-07-02 17:30:01 -0700

    completing step 7

--- song4u/index.js
+++ song4u/index.js
@@ -1,18 +1,74 @@
 const queryString = require('querystring');
+const fetch = require('node-fetch');
 
+/* 
+Initialize the request body -> parse it -> make a fetch request (GET) ->
+receive the data in the form of a buffer -> analyze the given image -> 
+get our result in the first index
+*/
 module.exports = async function (context, req) {
     // Ex. of reqbody output: 
     // 'ToCountry=US&MediaContentType0=image%2Fjpeg&ToState=MI&SmsMessageSid=MM0fe83458b74a1f626eb0da4685ab28b5&NumMedia=1'
     const reqBody = req.body;
-    console.log('reqBody:', reqBody);
+    context.log('reqBody:', reqBody);
 
     // we need to parse reqbody
     const queryObject = queryString.parse(reqBody);
-    console.log('queryObject:', queryObject);
+    context.log('queryObject:', queryObject);
+
+    let resp = await fetch(queryObject.MediaUrl0, {
+        method: 'GET',
+    });
+
+    // receive the response
+    let data = await resp.arrayBuffer();
+    // we are receiving it as a Buffer since this is binary data
+
+    let result = analyzeImage(data);
+
+    let age = result[0].faceAttributes.age;
+
+    let generation = '';
+
+    if (age > 5 && age < 25) {
+        generation = 'GenZ';
+    } else if (age > 24 && age < 41) {
+        generation = 'GenY';
+    } else if (age > 40 && age < 57) {
+        generation = 'GenX';
+    } else if (age > 56 && age < 76) {
+        generation = 'BabyBoomers';
+    } else {
+        generation = 'Any other age';
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: queryObject.MediaUrl0
+        body: generation
     };
 }
 
+async function analyzeImage(image) {
+    // subscription key and endpoint
+    const subscriptKey = process.env.SUBSCRIPTIONKEY;
+    const theUrl = process.env.ENDPOINT + '/face/v1.0/detect';
+
+    // establish the parameters 
+    let theParameters = new URLSearchParams({
+        'returnFaceId': true,
+        'returnFaceAttributes': age
+    });
+
+    let response = fetch(theUrl + '?' + theParameters.toString(), {
+        method: 'POST',
+        body: image,  //WHAT ARE WE SENDING TO THE API?
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey //do this in the next section
+        }
+    });
+
+    let theData = await response.json();
+    
+    return theData;
+}
