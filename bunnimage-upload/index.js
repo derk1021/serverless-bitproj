c32eb0e38ada79dbf5d62fed1adbe7f2e2dab704
derk1021 <derekbl@uci.edu>
2021-07-05 19:24:33 -0700

    Completing Step 2

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,10 +1,29 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const { BlobServiceClient } = require("@azure/storage-blob");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+module.exports = async function (context, req) {
+    // Parsing the data
+    const body = req.body;
+    const boundary = multipart.getBoundary(req.headers['content-type']);
+    const parsedBody = multipart.Parse(body, boundary);
+
+    // checking what the file type is
+    let filetype = parsedBody[0].type;
+    let ext = "";
+    if (filetype == "image/png") {
+        ext = "png";
+    } else if (filetype == "image/jpeg") {
+        ext = "jpeg";
+    } else if (filetype == "image/jpg") {
+        ext = "jpg";
+    } else {
+        username = "invalidimage";
+    }
+    
+    // calling the upload file function
+    let responseMessage = await uploadFile(parsedBody, ext);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -12,3 +31,24 @@ module.exports = async function (context, req) {
     };
 }
 
+// Function for uploading a file to the blob storage
+async function uploadFile(parsedBody, ext) {
+    /*
+    These three lines basically tell blob "Hey, these are my credentials,
+    can you let me access my storage?"
+    */
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    const containerName = "blob-container";
+    const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
+    
+    // Creating a blob
+    // ext == extension, like .jpeg or .png
+    const blobName = 'test.' + ext;    // Create the container, or available directories
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client, or getting the files ready
+    
+    // Uploading the blob to the container
+    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
+
+    // we don't return the upload because we are saving it in this function
+    return "File Saved!"
+}
