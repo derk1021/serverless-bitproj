3f02ff757c20cf2a9b905e1563c8231c2ec59b55
derk1021 <derekbl@uci.edu>
2021-07-08 15:10:19 -0700

    Step 8

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -1,4 +1,4 @@
-/******** Store a message from Twilio in CosmosDB and return the most recent message *********/
+/******** Code: Store a message from Twilio in CosmosDB and return the most recent message *********/
 const querystring = require('querystring');
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 // Create a config object that contains all of the sensitive information that we need to manipulate our data.
@@ -11,6 +11,14 @@ const config = {
     };
 
 module.exports = async function (context, req) {
+    /******** Code: Twilio phone number texts your message back *********/
+    /*
+    const queryObject = querystring.parse(req.body);
+    context.res = {
+        body: responseMessage
+    };
+    */
+
     // Parsing the body
     const queryObject = querystring.parse(req.body);
     // Storing the acutual message
@@ -19,9 +27,11 @@ module.exports = async function (context, req) {
     let document = {"message" : message};
     // Store the message in a document
     let items = await createDocument(document);
-    context.log('items:', items + '\n' + 'items[0]:', items[0]);
+    // Generating a random integer that is strictly less than the length of the array of secrets
+    const generateRandomSecret = Math.floor(Math.random() * items.length);
+    context.log('items:', items + '\n' + 'items[generateRandomSecret]:', items[generateRandomSecret]);
 
-    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[0].message)}`
+    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[generateRandomSecret].message)}`
 
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -64,8 +74,11 @@ async function createDocument(newItem) {
     // Create and execute a query that uses SQL language to fetch the most recent data document.
     const querySpec = {
         // This SQL query requests for the "top 1" document when it is orderd by c._ts, or the timestamp, in descending order.
-        query: "SELECT top 1 * FROM c order by c._ts desc"
+        // query: "SELECT top 1 * FROM c order by c._ts desc"
+        // Query for all secrets inside your container.
+        query: "SELECT * from c"
     };
+    console.log('queries: ', querySpec.query);
 
     // Using the query querySpec, it will use the container we created to fetch the most recent document!
     const { resources: items } = await container.items.query(querySpec).fetchAll();
