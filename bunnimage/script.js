16e2aa078d5eddf98c0390e5cff69056c45abbbe
derk1021 <derekbl@uci.edu>
2021-07-13 19:54:50 -0700

    Step 2

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,5 +1,13 @@
-function getImage() {
+function getImage(event) {
+    /*
     if (document.getElementById('name').value !== '') {
         $('#output').text(document.getElementById('name').value + "❤️");
+    }
+    */
+    event.preventDefault(); // stops the page from refreshing
+    if (document.getElementById('name').value !== '') {
+        $('#output').text('Thanks!');
+    } else {
+        alert('No name error.')
     }
 }
