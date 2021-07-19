5219cfe17597b909f870448f4c0a0c21b495c75e
derk1021 <derekbl@uci.edu>
2021-07-18 18:40:42 -0700

    Step 7

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,6 +1,12 @@
 function y1k3s() {
-    let inputSomething = document.getElementById("inputtext").value;
-    if (inputSomething !== "") {
-        document.getElementById("image").src = `https://cataas.com/cat/says/${inputSomething}`;
+    let name1 = document.getElementById("name1").value;
+    let name2 = document.getElementById("name2").value;
+    let name3 = document.getElementById("name3").value;
+    let name4 = document.getElementById("name4").value;
+    if (name1 !== "" && name2 !== "" && name3 !== "" && name4 !== "") {
+        document.getElementById("image1").src = `https://cataas.com/cat/says/${name1}`;
+        document.getElementById("image2").src = `https://cataas.com/cat/says/${name2}`;
+        document.getElementById("image3").src = `https://cataas.com/cat/says/${name3}`;
+        document.getElementById("image4").src = `https://cataas.com/cat/says/${name4}`;
     }
 }
